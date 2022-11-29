05eebbc312bf19fdcca10cae401a52babf475cc7
trung <trungdavid969@gmail.com>
2022-11-29 23:25:17 +0100

    reconstruct js by using innerHTML

--- src/js/app.js
+++ src/js/app.js
@@ -1,40 +1,36 @@
 import contacts from "./modules/contactsList.js";
-import createNode from "./modules/createNode.js";
+
+const personContent = document.querySelector(".contacts ul"),
+    submitBtn = document.querySelector("#btn-submit");
+
+const search = document.querySelector("#search");
+
+let value = "";
+
+function submit(e) {
+    e.preventDefault();
+    let items = document.querySelectorAll(".contacts ul li");
+
+    contacts.filter((person, i) => {
+        if (person.name.toUpperCase().indexOf(value) > -1) {
+            items[i].style.display = "";
+        } else {
+            items[i].style.display = "none";
+        }
+    });
+    search.value = "";
+}
 
 function showPeople() {
-  const personContent = document.querySelector(".contacts ul");
-
-  contacts.map((person) => {
-    const contact = createNode({
-      element: document.createElement("li"),
-      className: "person",
-      children: [
-        createNode({
-          element: document.createElement("img"),
-          props: {
-            src: person.img,
-          },
-        }),
-        createNode({
-          element: document.createElement("div"),
-          className: "person-info",
-          children: [
-            createNode({
-              element: document.createElement("span"),
-              className: "name",
-              innerText: person.name,
-            }),
-            createNode({
-              element: document.createElement("span"),
-              className: "tel",
-              innerText: person.tel,
-            }),
-          ],
-        }),
-      ],
+    contacts.map((person) => {
+        personContent.innerHTML += `<li class="person"><img src="${person.img}"/><div class="person-info"><span class="name">${person.name}</span><span class="tel">${person.tel}</span></div></li>`;
     });
-    personContent.appendChild(contact);
-  });
 }
+
+search.addEventListener("keyup", () => {
+    value = search.value.toUpperCase();
+});
+
+submitBtn.addEventListener("click", submit);
 
 showPeople();
