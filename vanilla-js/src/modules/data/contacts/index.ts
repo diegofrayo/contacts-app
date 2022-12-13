d4e9e25b354e384416316680db417f9d52d9acad
Diego Rayo <diegofrayo@gmail.com>
2022-12-13 16:46:14 -0500

    :bug: [fix] a build issue on vanilla-js project

--- vanilla-js/src/modules/data/contacts/index.ts
+++ vanilla-js/src/modules/data/contacts/index.ts
@@ -3,3 +3,5 @@ import StrategyWithLocalStorage from "./StrategyWithLocalStorage";
 // import StrategyWithFirebase from "./StrategyWithFirebase";
 
 export default new ContactsContext(new StrategyWithLocalStorage());
+
+export type { T_Contact, T_ContactObject, T_CreateContactDTO } from "./Model";
